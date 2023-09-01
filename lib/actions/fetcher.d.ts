2081d682d400358f42360b026effe9ae78fb8a0e
gaurav6386 <60260013+gaurav6386@users.noreply.github.com>
2023-09-01 14:15:14 +0530

    removed dist code from remote repo

--- lib/actions/fetcher.d.ts
+++
@@ -1,9 +0,0 @@
-import { AllStsRecordSchema } from '../types';
-/**
- * Fetch the asked DNS record and check if it is valid or not
- *
- * @param domainName string
- * @param recordType string
- * @returns
- */
-export declare function fetcher(domainName: string, recordType: string): Promise<string | AllStsRecordSchema>;
